3923f54d603ff56bcdee8bbe5d0a9b37373d3c99
Ron Britzius <thebritz41@gmail.com>
2024-12-30 15:18:31 -0500

    Adding more robust output

--- server_properties.gd
+++ server_properties.gd
@@ -8,9 +8,10 @@ func _ready() -> void:
 		print("Server properties created")
 	else:
 		print("Remove server properties synced")
-	label = get_node("ServerPropertiesDisplay") as RichTextLabel
-	label.text = PlayerSpeed
-
+	#label = get_node("ServerPropertiesDisplay")
+	label = get_child(1) as RichTextLabel
+	label.text = str(PlayerSpeed)
+	
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_up"):
 		PlayerSpeed += 25
@@ -18,4 +19,10 @@ func _input(event: InputEvent) -> void:
 		PlayerSpeed -= 25
 
 func _process(delta: float) -> void:
-	label.text = PlayerSpeed
+	label.text = ""
+	var thisScript: GDScript = get_script()
+	for propertyInfo in thisScript.get_script_property_list():
+		var propertyName: String = propertyInfo.name
+		var propertyValue = get(propertyName)
+		label.text = label.text + propertyName + " = " + str(propertyValue) + "\n"
+	
