d2736aeb8a5d20517887272b1c3367fe6e357697
Ron Britzius <thebritz41@gmail.com>
2024-12-30 14:31:00 -0500

    adding test label for server properties

--- basic_player.gd
+++ basic_player.gd
@@ -5,8 +5,6 @@ var serverProperties: Node
 func _enter_tree():
 	set_multiplayer_authority(name.to_int())
 	serverProperties = get_node("../ServerProperties")
-	print(serverProperties)
-	print(serverProperties.PlayerSpeed)
 
 func _physics_process(delta):
 	if is_multiplayer_authority():
--- server_properties.gd
+++ server_properties.gd
@@ -1,15 +1,21 @@
 extends Node
 
 @export var PlayerSpeed = 400
+var label: RichTextLabel = null
 
 func _ready() -> void:
 	if multiplayer.is_server:
 		print("Server properties created")
 	else:
 		print("Remove server properties synced")
+	label = get_node("ServerPropertiesDisplay") as RichTextLabel
+	label.text = PlayerSpeed
 
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("ui_up"):
 		PlayerSpeed += 25
 	if Input.is_action_just_pressed("ui_down"):
 		PlayerSpeed -= 25
+
+func _process(delta: float) -> void:
+	label.text = PlayerSpeed
