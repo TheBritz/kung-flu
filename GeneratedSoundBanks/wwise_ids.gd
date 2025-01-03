3636a77829985026cd44a0c94913746036f0a001
alexlehmanmusic <133513811+alexlehmanmusic@users.noreply.github.com>
2025-01-02 23:33:55 -0500

    Wwise integration (#1)
    * Implementing Wwise-Godot integration
    
    * Adding integration files
    
    * Removing addons directory
    
    * Adding addons to gitignore

---
+++ GeneratedSoundBanks/wwise_ids.gd
@@ -0,0 +1,120 @@
+class_name AK
+
+class EVENTS:
+
+	const TESTSOUND: int = 38624190
+	const STATICSOUND: int = 3740254892
+
+	const _dict: Dictionary = {
+		"TestSound": TESTSOUND,
+		"StaticSound": STATICSOUND
+	}
+
+class STATES:
+
+	const _dict: Dictionary = {}
+
+class SWITCHES:
+
+	const _dict: Dictionary = {}
+
+class GAME_PARAMETERS:
+
+	const SS_AIR_RPM: int = 822163944
+	const SS_AIR_SIZE: int = 3074696722
+	const SS_AIR_TURBULENCE: int = 4160247818
+	const SS_AIR_STORM: int = 3715662592
+	const SS_AIR_FEAR: int = 1351367891
+	const SS_AIR_FREEFALL: int = 3002758120
+	const SS_AIR_PRESENCE: int = 3847924954
+	const SS_AIR_TIMEOFDAY: int = 3203397129
+	const SS_AIR_MONTH: int = 2648548617
+	const SS_AIR_FURY: int = 1029930033
+
+	const _dict: Dictionary = {
+		"SS_Air_RPM": SS_AIR_RPM,
+		"SS_Air_Size": SS_AIR_SIZE,
+		"SS_Air_Turbulence": SS_AIR_TURBULENCE,
+		"SS_Air_Storm": SS_AIR_STORM,
+		"SS_Air_Fear": SS_AIR_FEAR,
+		"SS_Air_Freefall": SS_AIR_FREEFALL,
+		"SS_Air_Presence": SS_AIR_PRESENCE,
+		"SS_Air_TimeOfDay": SS_AIR_TIMEOFDAY,
+		"SS_Air_Month": SS_AIR_MONTH,
+		"SS_Air_Fury": SS_AIR_FURY
+	}
+
+class TRIGGERS:
+
+	const _dict: Dictionary = {}
+
+class BANKS:
+
+	const INIT: int = 1355168291
+	const SCENE01: int = 340227046
+
+	const _dict: Dictionary = {
+		"Init": INIT,
+		"Scene01": SCENE01
+	}
+
+class BUSSES:
+
+	const MASTER_AUDIO_BUS: int = 3803692087
+
+	const _dict: Dictionary = {
+		"Master Audio Bus": MASTER_AUDIO_BUS
+	}
+
+class AUX_BUSSES:
+
+	const _dict: Dictionary = {}
+
+class AUDIO_DEVICES:
+
+	const SYSTEM: int = 3859886410
+	const NO_OUTPUT: int = 2317455096
+
+	const _dict: Dictionary = {
+		"System": SYSTEM,
+		"No_Output": NO_OUTPUT
+	}
+
+class EXTERNAL_SOURCES:
+
+	const _dict: Dictionary = {}
+
+class ACOUSTIC_TEXTURES:
+
+	const BRICK: int = 504532776
+	const MOUNTAIN: int = 513139656
+	const ANECHOIC: int = 1873957695
+	const CURTAINS: int = 2928161104
+	const CONCRETE: int = 841620460
+	const DRYWALL: int = 3670307564
+	const FABRIC: int = 1970351858
+	const WOOD_BRIGHT: int = 4262522749
+	const ACOUSTIC_BANNER: int = 4168643977
+	const WOOD: int = 2058049674
+	const CORK_TILES: int = 3195498748
+	const WOOD_DEEP: int = 1755085759
+	const TILE: int = 2637588553
+	const CARPET: int = 2412606308
+
+	const _dict: Dictionary = {
+		"Brick": BRICK,
+		"Mountain": MOUNTAIN,
+		"Anechoic": ANECHOIC,
+		"Curtains": CURTAINS,
+		"Concrete": CONCRETE,
+		"Drywall": DRYWALL,
+		"Fabric": FABRIC,
+		"Wood_Bright": WOOD_BRIGHT,
+		"Acoustic_Banner": ACOUSTIC_BANNER,
+		"Wood": WOOD,
+		"Cork_Tiles": CORK_TILES,
+		"Wood_Deep": WOOD_DEEP,
+		"Tile": TILE,
+		"Carpet": CARPET
+	}
+
